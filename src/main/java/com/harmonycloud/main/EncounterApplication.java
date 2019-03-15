f124953af65a5d75a84ee4324d95c1fd68564d90
dongqi <dongqi@harmonycloud.cn>
2019-03-15 16:05:06 +0800

    update

--- src/main/java/com/harmonycloud/main/EncounterApplication.java
+++ src/main/java/com/harmonycloud/main/EncounterApplication.java
@@ -4,6 +4,8 @@ import org.apache.servicecomb.saga.omega.spring.EnableOmega;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -14,7 +16,12 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableJpaRepositories("com.harmonycloud.repository")
 @EntityScan("com.harmonycloud.entity")
 @EnableSwagger2
-public class EncounterApplication {
+public class EncounterApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(this.getClass());
+    }
     public static void main(String[] args) {
         SpringApplication.run(EncounterApplication.class, args);
     }
