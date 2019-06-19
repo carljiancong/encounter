b15c9ecb4f0c37fa473206e5fced2a5abf1e17d1
jiancongc <carl.chen@asiasoft.hk>
2019-06-19 14:47:50 +0800

    commit.

--- src/main/java/com/harmonycloud/security/JwtAuthenticationFilter.java
+++ src/main/java/com/harmonycloud/security/JwtAuthenticationFilter.java
@@ -1,4 +1,4 @@
-/*package com.harmonycloud.security;
+package com.harmonycloud.security;
 
 import com.harmonycloud.bo.UserPrincipal;
 import com.harmonycloud.enums.ErrorMsgEnum;
@@ -52,4 +52,3 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         return null;
     }
 }
-*/
